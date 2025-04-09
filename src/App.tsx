1e346cbcd249bd1a6b50cbd82970ce4db6836572
PhilReact <philliplangmartinez@gmail.com>
2025-04-09 23:05:18 +0300

    update library qapp-core

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const navigate = useNavigate();
 
 const handleSearch = () => {
   if (search.trim()) {
-    navigate(`/search?q=${encodeURIComponent(search.trim())}`);
+    navigate(`/search?q=${encodeURIComponent(search.trim().toLocaleLowerCase())}`);
   }
 };
 
